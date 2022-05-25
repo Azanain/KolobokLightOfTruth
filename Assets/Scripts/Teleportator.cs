f97bafaa01d76f766a75be0a718f65516ca08b1f
Azanain <101041185+Azanain@users.noreply.github.com>
2022-05-25 23:36:51 +0500

    Лазер сделан

--- Assets/Scripts/Teleportator.cs
+++ Assets/Scripts/Teleportator.cs
@@ -1,25 +1,35 @@
+using System.Collections;
 using UnityEngine;
 
 public class Teleportator : MonoBehaviour
 {
     [SerializeField] private GameObject capsule;
-    
+    private float positionCapsuleY;
     private Transform playerPosition;
-    private GameObject player;
-    private bool isCaused;
+    private Transform player;
+    [SerializeField] private GameObject botPosition;
+    [SerializeField] private Rigidbody rb;
     private void Start()
     {
         EventManager.CallCapsuleTeleportEvent += CapsuleCall;
-        player = GameObject.FindGameObjectWithTag("Player");
+        player = GameObject.FindGameObjectWithTag("Player").transform;
     }
     private void CapsuleCall()
     {
-        if (!isCaused)
-        { 
-            playerPosition = player.transform;
-            float positionCapsuleY = playerPosition.transform.position.y + 12f;
-            capsule.transform.position = new Vector3(playerPosition.transform.position.x, positionCapsuleY, playerPosition.transform.position.z);
-            capsule.SetActive(true);
+        EventManager.CanMove();
+        playerPosition = player.transform;
+        botPosition.transform.position = new Vector3(player.position.x, player.position.y +4, player.position.z);
+        botPosition.SetActive(true);
+        positionCapsuleY = playerPosition.position.y + 35f;
+        capsule.transform.position = new Vector3(playerPosition.position.x, positionCapsuleY, playerPosition.position.z);
+        capsule.SetActive(true);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "botPosition")
+        {
+            rb.useGravity = false;
+            Debug.Log("afafs");
         }
     }
     private void OnDestroy()
